aa94aadd8c954c313d15db5a85f822f72161a1b4
Dmitri611 <dimkanov543@gmail.com>
2022-05-24 15:38:16 +0300

    feat: update

--- src/pages/promo-codes/promoCodes.jsx
+++ src/pages/promo-codes/promoCodes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "components/button/button";
 import Section from "components/section/section";
@@ -11,9 +11,56 @@ import TableCardItemTh from "components/table/components/tableCardItem/tableCard
 import bundle from "uploads/promo-codes/bundle.webp";
 import { ReactComponent as Edit } from "assets/image/svg/draw.svg";
 import { ReactComponent as Delete } from "assets/image/svg/delete.svg";
+import FormCard from "components/form/components/form-card/form-card";
+import ModalEdit from "components/modalEdit/modalEdit";
+import ModalWarn from "components/modalWarn/modalWarn";
+import Picture from "components/picture/picture";
+import modalAddStyles from "components/modalEdit/modalEdit.module.scss";
+
 
 const PromoCodes = () => {
+
+  const [defaultSrc, setDefaultSrc] = useState(bundle);
+
+  const def = (e) => {
+    const src = URL.createObjectURL(e.target.files[0]);
+    setDefaultSrc(src);
+  };
+
+  const [imageUrl, setImageUrl] = useState();
+
+  const change = (e) => {
+    const src = URL.createObjectURL(e.target.files[0]);
+    setImageUrl(src);
+  };
+
+  const [modalAddStyle, setModalAddStyle] = useState(null);
+  const [modalEditStyle, setModalEditStyle] = useState(null);
+  const [modalWarnStyle, setModalWarnStyle] = useState(null);
+
+  const style = "modal-display";
+
+  const addPromo = () => {
+    setModalAddStyle(style);
+  };
+
+  const editPromo = () => {
+    setModalEditStyle(style);
+  };
+
+  const modalWarn = () => {
+    setModalWarnStyle(style);
+  };
+
+  const close = () => {
+    setModalAddStyle(null);
+    setModalWarnStyle(null);
+    setModalEditStyle(null);
+  };
+
+
   return (
+    <>
     <Section className="section__inner--size-s" title="Промо коды">
       <SectionContent>
         <Table>
@@ -29,20 +76,18 @@ const PromoCodes = () => {
           <TableCard>
             <TableCardItem text="1" />
             <TableCardItem>
-              <picture>
-                <img src={bundle} alt="bundle" />
-              </picture>
+              <Picture src={bundle} />
             </TableCardItem>
             <TableCardItem text="NEW-EDA" />
             <TableCardItem text="Первая пицца бесплатно" />
             <TableCardItem text="100%" />
             <TableCardItem>
-              <Button className="button--edit">
+              <Button handler={editPromo} className="button--edit">
                 <Edit />
               </Button>
             </TableCardItem>
             <TableCardItem>
-              <Button className="button--edit">
+              <Button handler={modalWarn} className="button--edit">
                 <Delete />
               </Button>
             </TableCardItem>
@@ -52,10 +97,44 @@ const PromoCodes = () => {
           <Link to="/admin">
             <Button className="button--size-m" text="Назад" />
           </Link>
-          <Button className="button--default" text="Добавить" />
+          <Button handler={addPromo} className="button--default" text="Добавить" />
         </SectionFooter>
       </SectionContent>
     </Section>
+    <ModalWarn display={modalWarnStyle} handler={close} />
+    <ModalEdit display={modalAddStyle} title="Добавить промо код" handler={close}>
+    <Picture className={modalAddStyles["modal-edit__image"]} src={imageUrl} />
+        <label htmlFor="inputqq">
+          <span>Добавить бандл</span>
+          <input
+            onChange={change}
+            id="inputqq"
+            className={modalAddStyles["modal-edit__input"]}
+            type="file"
+          />
+        </label>
+      <FormCard title="Код" type="text" placeholder="Код" />
+      <FormCard title="Название" type="text" placeholder="Название" />
+      <FormCard title="Скидка" type="email" placeholder="%" />
+      <Button type="submit" className="button--size-m" text="Добавить" />
+    </ModalEdit>
+    <ModalEdit display={modalEditStyle} title="Изменить промо код" handler={close}>
+    <Picture className={modalAddStyles["modal-edit__image"]} src={defaultSrc} />
+        <label htmlFor="inputqq">
+          <span>Изменить бандл</span>
+          <input
+            onChange={def}
+            id="inputqq"
+            className={modalAddStyles["modal-edit__input"]}
+            type="file"
+          />
+        </label>
+      <FormCard title="Код" type="text" placeholder="Код" />
+      <FormCard title="Название" type="text" placeholder="Название" />
+      <FormCard title="Скидка" type="email" placeholder="%" />
+      <Button type="submit" className="button--size-m" text="изменить" />
+    </ModalEdit>
+    </>
   );
 };
 
--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "components/button/button";
 import Section from "components/section/section";
@@ -10,51 +10,99 @@ import TableCardItem from "components/table/components/tableCardItem/tableCardIt
 import TableCardItemTh from "components/table/components/tableCardItem/tableCardItemTh";
 import { ReactComponent as Edit } from "assets/image/svg/draw.svg";
 import { ReactComponent as Delete } from "assets/image/svg/delete.svg";
+import ModalWarn from "components/modalWarn/modalWarn";
+import ModalEdit from "components/modalEdit/modalEdit";
+import FormCard from "components/form/components/form-card/form-card";
 
 const Users = () => {
+  const [modalAddStyle, setModalAddStyle] = useState(null);
+  const [modalEditStyle, setModalEditStyle] = useState(null);
+  const [modalWarnStyle, setModalWarnStyle] = useState(null);
+
+  const style = "modal-display";
+
+  const addUser = () => {
+    setModalAddStyle(style);
+  };
+
+  const editUser = () => {
+    setModalEditStyle(style);
+  };
+
+  const modalWarn = () => {
+    setModalWarnStyle(style);
+  };
+
+  const close = () => {
+    setModalAddStyle(null);
+    setModalWarnStyle(null);
+    setModalEditStyle(null);
+  };
+
   return (
-    <Section className="section__inner--size-s" title="Пользователи">
-      <SectionContent>
-        <Table>
-          <TableCard>
-            <TableCardItemTh text="№" />
-            <TableCardItemTh text="Статус" />
-            <TableCardItemTh text="Имя" />
-            <TableCardItemTh text="Фамилия" />
-            <TableCardItemTh text="Почта" />
-            <TableCardItemTh text="Телефон" />
-            <TableCardItemTh text="Пароль" />
-            <TableCardItemTh text="Изменить" />
-            <TableCardItemTh text="Удалить" />
-          </TableCard>
-          <TableCard>
-            <TableCardItem text="1" />
-            <TableCardItem text="admin" />
-            <TableCardItem text="Дмитрий" />
-            <TableCardItem text="Новиков" />
-            <TableCardItem text="dimkanov543@gmail.com" />
-            <TableCardItem text="+375333910514" />
-            <TableCardItem text="12345" />
-            <TableCardItem>
-              <Button className="button--edit">
-                <Edit />
-              </Button>
-            </TableCardItem>
-            <TableCardItem>
-              <Button className="button--edit">
-                <Delete />
-              </Button>
-            </TableCardItem>
-          </TableCard>
-        </Table>
-        <SectionFooter>
-          <Link to="/admin">
-            <Button className="button--size-m" text="Назад" />
-          </Link>
-          <Button className="button--default" text="Добавить" />
-        </SectionFooter>
-      </SectionContent>
-    </Section>
+    <>
+      <Section className="section__inner--size-s" title="Пользователи">
+        <SectionContent>
+          <Table>
+            <TableCard>
+              <TableCardItemTh text="№" />
+              <TableCardItemTh text="Статус" />
+              <TableCardItemTh text="Имя" />
+              <TableCardItemTh text="Фамилия" />
+              <TableCardItemTh text="Почта" />
+              <TableCardItemTh text="Телефон" />
+              <TableCardItemTh text="Пароль" />
+              <TableCardItemTh text="Изменить" />
+              <TableCardItemTh text="Удалить" />
+            </TableCard>
+            <TableCard>
+              <TableCardItem text="1" />
+              <TableCardItem text="admin" />
+              <TableCardItem text="Дмитрий" />
+              <TableCardItem text="Новиков" />
+              <TableCardItem text="dimkanov543@gmail.com" />
+              <TableCardItem text="+375333910514" />
+              <TableCardItem text="12345" />
+              <TableCardItem>
+                <Button handler={editUser} className="button--edit">
+                  <Edit />
+                </Button>
+              </TableCardItem>
+              <TableCardItem>
+                <Button handler={modalWarn} className="button--edit">
+                  <Delete />
+                </Button>
+              </TableCardItem>
+            </TableCard>
+          </Table>
+          <SectionFooter>
+            <Link to="/admin">
+              <Button className="button--size-m" text="Назад" />
+            </Link>
+            <Button handler={addUser} className="button--default" text="Добавить" />
+          </SectionFooter>
+        </SectionContent>
+      </Section>
+      <ModalWarn display={modalWarnStyle} handler={close} />
+      <ModalEdit display={modalAddStyle} title="Добавить пользователя" handler={close}>
+        <FormCard title="Роль" type="text" placeholder="Статус" />
+        <FormCard title="Имя" type="text" placeholder="Имя" />
+        <FormCard title="Фамилия" type="text" placeholder="Фамилия" />
+        <FormCard title="Почта" type="email" placeholder="Почта" />
+        <FormCard title="Номер телефона" type="tel" placeholder="Номер телефона" />
+        <FormCard title="Пароль" type="text" placeholder="Пароль" />
+        <Button type="submit" className="button--size-m" text="Добавить" />
+      </ModalEdit>
+      <ModalEdit display={modalEditStyle} title="Изменить пользователя" handler={close}>
+        <FormCard title="Роль" type="text" placeholder="Статус" /> 
+        <FormCard title="Имя" type="text" placeholder="Имя" />
+        <FormCard title="Фамилия" type="text" placeholder="Фамилия" />
+        <FormCard title="Почта" type="email" placeholder="Почта" />
+        <FormCard title="Номер телефона" type="tel" placeholder="Номер телефона" />
+        <FormCard title="Пароль" type="text" placeholder="Пароль" />
+        <Button type="submit" className="button--size-m" text="изменить" />
+      </ModalEdit>
+    </>
   );
 };
 
